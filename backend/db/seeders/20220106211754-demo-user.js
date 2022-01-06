7b3e889aa19b266ce73a5944728510f85621174e
App Academy <sure777.shz@gmail.com>
2022-01-06 14:53:52 -0800

    first commit

---
+++ backend/db/seeders/20220106211754-demo-user.js
@@ -0,0 +1,31 @@
+'use strict';
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [
+      {
+        email: 'demo@user.io',
+        username: 'Demo-lition',
+        hashedPassword: bcrypt.hashSync('password'),
+      },
+      {
+        email: 'John@user.io',
+        username: 'John',
+        hashedPassword: bcrypt.hashSync('password'),
+      },
+      {
+        email: 'Harry@user.io',
+        username: 'Harry',
+        hashedPassword: bcrypt.hashSync('password'),
+      },
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete('Users', {
+      username: { [Op.in]: ['Demo-lition', 'John', 'Harry'] }
+    }, {});
+  }
+};
